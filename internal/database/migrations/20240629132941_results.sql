5c3ab1c778b516323db31cce513054fe82ed5311
Danila Beloglazov <denbeliyzxc@gmail.com>
2024-06-30 15:10:42 +0300

    chore(migration): add new columns to results table

--- internal/database/migrations/20240629132941_results.sql
+++ internal/database/migrations/20240629132941_results.sql
@@ -7,9 +7,11 @@ CREATE TABLE results (
     game_mode VARCHAR(50) NOT NULL,
     start_time TIMESTAMP DEFAULT TO_CHAR(CURRENT_TIMESTAMP AT TIME ZONE 'MSK', 'YYYY-MM-DD HH24:MI:SS')::TIMESTAMP,
     duration time NOT NULL,
-    misstakes int NOT NULL,
+    mistakes int NOT NULL,
     accuracy FLOAT NOT NULL,
     count_words int NOT NULL,
+    wpn FLOAT NOT NULL,
+    cpn FLOAT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 -- +goose StatementEnd
